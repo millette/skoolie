b9657a45110152a069c209ebe2868f5f211d0eed
Robin Millette <robin@millette.info>
2023-06-19 00:44:24 -0400

    Remove typography and daisyui tailwindcss plugins.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,14 @@
 // npm
 /** @type {import('tailwindcss').Config} */
-const daisyui = require("daisyui")
-const typography = require("@tailwindcss/typography")
+// const daisyui = require("daisyui")
+// const typography = require("@tailwindcss/typography")
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mdoc}"],
+  /*
   plugins: [
     typography,
     daisyui,
   ],
+  */
 }
