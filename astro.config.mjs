b13ee643ed13f524a3c7cbf3c25abbe3e0227bef
Robin Millette <robin@millette.info>
2023-07-15 19:29:15 -0400

    sitemap: Ignore under album.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import prefetch from '@astrojs/prefetch'
 
+const site = 'https://bus.waglo.com'
+
 export default defineConfig({
-  site: 'https://bus.waglo.com',
+  site,
   compressHTML: true,
   trailingSlash: 'always',
   experimental: {
@@ -18,7 +20,9 @@ export default defineConfig({
   integrations: [
     markdoc(),
     prefetch({ selector: "a" }),
-    sitemap(),
+    sitemap({
+      filter: (page) => !page.startsWith(`${site}/en/album/under/`) && !page.startsWith(`${site}/album/under/`)
+    }),
     tailwind(),
   ],
 })
