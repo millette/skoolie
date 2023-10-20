4584d80bef1fece4488f1c1ee1978f7cff8cf496
Robin Millette <robin@millette.info>
2023-10-20 19:02:00 -0400

    Add node backend (adaptor).

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,14 @@ import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import prefetch from '@astrojs/prefetch'
 import solid from '@astrojs/solid-js'
+import node from '@astrojs/node'
 
 const site = 'https://bus.waglo.com'
 
 export default defineConfig({
   site,
+  output: 'hybrid',
+  adapter: node({ mode: 'standalone' }),
   compressHTML: true,
   trailingSlash: 'always',
   integrations: [
