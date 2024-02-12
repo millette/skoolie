c5b1554efb3471eed34bcd6743cc186160759c76
Robin Millette <robin@millette.info>
2024-02-12 10:47:35 -0500

    Clean-up content config.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,9 @@
-// 1. Import utilities from `astro:content`
+// astro
 import { z, defineCollection } from 'astro:content'
 
-// 2. Define a `type` and `schema` for each collection
-const pageCollection = defineCollection({
-  type: 'content', // v2.5.0 and later
-  schema: z.object({
-    title: z.string(),
-    // tags: z.array(z.string()),
-    // image: z.string().optional(),
-  }),
+const pages = defineCollection({
+  type: 'content',
+  schema: z.object({ title: z.string() }),
 })
 
-// 3. Export a single `collections` object to register your collection(s)
-export const collections = {
-  'pages': pageCollection,
-}
+export const collections = { pages }
