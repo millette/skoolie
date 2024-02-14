009cf4e17a2bccf4b778e40b831108f83059fe2b
Robin Millette <robin@millette.info>
2024-02-14 02:27:09 -0500

    Use animate tailwind plugin.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,14 @@
 /** @type {import('tailwindcss').Config} */
 const daisyui = require("daisyui")
 const typography = require("@tailwindcss/typography")
+const animate = require("tailwindcss-animated")
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mdoc}"],
   plugins: [
     typography,
     daisyui,
+    animate,
   ],
   daisyui: {
     themes: [
