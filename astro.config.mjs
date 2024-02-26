ec58293a9afd3cb2175c1b34c2f8b8296f3f6d48
Robin Millette <robin@millette.info>
2024-02-25 23:16:30 -0500

    Remove @astrojs/prefetch.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 // npm
-// import { defineConfig, sharpImageService } from 'astro/config'
 import { defineConfig } from 'astro/config'
 import markdoc from '@astrojs/markdoc'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
-import prefetch from '@astrojs/prefetch'
 import solid from '@astrojs/solid-js'
 
 const site = 'https://bus.waglo.com'
@@ -16,10 +14,6 @@ export default defineConfig({
   integrations: [
     markdoc(),
     solid(),
-    prefetch({
-      selector: "a[href$='/']", // only prefetch href ending with /
-      throttle: 4,
-    }),
     sitemap({
       filter: (page) => !page.startsWith(`${site}/en/album/under/`) && !page.startsWith(`${site}/album/under/`)
     }),
